4b6a8291aaafcc192e2324838bf042f229b3d46f
Theodor Dumitriu <mihaidumitriu@theodors-mbp.home>
2024-06-26 10:52:45 +0200

    feat: scaffolding: file structure & server root routea and 404 & server launch logic

---
+++ index.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  try {
+    console.log("Welcome to the cuteness overload server 🐱");
+    return res
+      .status(200)
+      .json({ message: "Welcome to the cuteness overload server 🐱" });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This route does not exist" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`Server started on PORT ${process.env.PORT} 🚀`);
+});
